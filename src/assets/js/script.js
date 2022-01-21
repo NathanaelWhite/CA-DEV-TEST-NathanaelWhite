71074376890f0e7d196e4e0a664b6779735156cb
Nathanael White <narhanaelw99@gmail.com>
2022-01-21 17:36:49 -0600

    cleaning up my code

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,22 +1,24 @@
 let imgIndex = 1;
-displaySlides(imgIndex);
 
 // shows the current image
-function currentImg(num) {
-  displaySlides((imgIndex = num));
+function currentImg(imgNum) {
+  displaySlides((imgIndex = imgNum));
 }
 
-function displaySlides(num) {
+// displays the images themselves
+function displaySlides(imgNum) {
   let images = document.getElementsByClassName("carousel-img");
   let buttons = document.getElementsByClassName("carousel-btn");
-
+    // loops over the images and 'hides' the styling
   for (i = 0; i < images.length; i++) {
     images[i].style.display = "none";
   }
-
+  // loops over the buttons and takes the 'active' className away
   for (i = 0; i < buttons.length; i++) {
     buttons[i].className = buttons[i].className.replace(" active", "");
   }
   images[imgIndex - 1].style.display = "block";
   buttons[imgIndex - 1].className += " active";
 }
+
+displaySlides(imgIndex);
